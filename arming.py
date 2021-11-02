98e40dc8f00f8a9c4bb75d9816ff77d49b2d90f7
arrown <80968603+arrown@users.noreply.github.com>
2021-11-02 23:39:01 +0900

    Create arming.py

---
+++ arming.py
@@ -0,0 +1,23 @@
+import time
+import math
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
+print("connecting to Vehicle")
+vehicle= connect("/dev/ttyACM0", baudrate = 57600, wait_ready=True)
+vehicle.airspeed = 0.3
+vehicle.groundspeed = 0.3
+time.sleep(1)
+def arming():
+    print ("Basic pre-arm checks")
+    while not vehicle.is_armable:
+        print("waiting for vehicle to initialise...")
+        time.sleep(1)
+    
+    print ("Arming motors")
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+  
+    while not vehicle.armed:
+        print("waiting for arming...")
+        time.sleep(1)
+   
+arming()
