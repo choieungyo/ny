976150f74ce42de8514a07a5dfe0ffc540d70247
arrown <80968603+arrown@users.noreply.github.com>
2021-11-08 16:01:51 +0900

    Update positioning.py

--- positioning.py
+++ positioning.py
@@ -166,7 +166,7 @@ def goto(dNorth, dEast, gotoFunction=vehicle.simple_goto):
         #print "DEBUG: mode: %s" % vehicle.mode.name
         remainingDistance=get_distance_metres(vehicle.location.global_relative_frame, targetLocation)
         print("Distance to target: ", remainingDistance)
-        if remainingDistance<=targetDistance*0.01: #Just below target, in case of undershoot.
+        if remainingDistance<=targetDistance*0.0001: #Just below target, in case of undershoot.
             print("Reached target")
             break;
         time.sleep(2)
